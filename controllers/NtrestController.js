f4464eb9c42079066f4893a047d22215b66d64fb
Ryan Garner <rpgarner22@gmail.com>
2022-05-13 07:13:45 -0600

    ready for heroku deployment

--- controllers/NtrestController.js
+++ controllers/NtrestController.js
@@ -6,38 +6,6 @@ const { User, Ntrest, Event } = require("../models");
 
 const { Op, literal, fn, col } = require("sequelize");
 
-// const GetPopularEvent = async (req, res) => {
-//   try {
-//     const popular = await Post.findAll({
-//       order: [["likes", "DESC"]],
-//       attributes: [
-//         "id",
-//         "description",
-//         "likes",
-//         "postName",
-//         "releaseDate",
-//         "images"[(fn("COUNT", col("comments.id")), "commentCount")],
-//       ],
-//       where: { likes: { [Op.gt]: 3 } },
-//       include: [
-//         { model: User, attributes: ["userName", "id"] },
-//         { model: Comment, attributes: [] },
-//       ],
-//     });
-//     res.send(popular);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const GetRecentEvents = async (req, res) => {
-//   try {
-//     const recents = await Event.findAll({ order: [["createdAt", "DESC"]] });
-//     res.json(recents);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 
 const GetAllNtrests = async (req, res) => {
     try {
--- routes/NtrestRouter.js
+++ routes/NtrestRouter.js
@@ -1,25 +1,25 @@
 const Router = require("express").Router();
 const controllers = require("../controllers/NtrestController");
-const middleware = require("../middleware");
+// const middleware = require("../middleware");
 
 Router.get("/ntrest", controllers.GetAllNtrests);
 Router.get("/:ntrestId", controllers.GetNtrestDetails);
 Router.post(
   "/:user_id",
-  middleware.stripToken,
-  middleware.verifyToken,
+//   middleware.stripToken,
+//   middleware.verifyToken,
   controllers.CreateNtrest
 );
 Router.put(
   "/:ntrest_id",
-  middleware.stripToken,
-  middleware.verifyToken,
+//   middleware.stripToken,
+//   middleware.verifyToken,
   controllers.UpdateNtrest
 );
 Router.delete(
   "/:ntrest_id",
-  middleware.stripToken,
-  middleware.verifyToken,
+//   middleware.stripToken,
+//   middleware.verifyToken,
   controllers.DeleteNtrest
 );
 
